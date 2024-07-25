793be27aa78bd8a22aea970ed786158471ae5a19
tafsir ihsani <tafsirihsani12@gmail.com>
2024-07-26 01:49:18 +0700

    fixing create user

--- resources/views/pages/users/create.blade.php
+++ resources/views/pages/users/create.blade.php
@@ -16,7 +16,7 @@
     <div class="main-content">
         <section class="section">
             <div class="section-header">
-                <h1>Advanced Forms</h1>
+                <h1>Tambah User</h1>
                 <div class="section-header-breadcrumb">
                     <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
                     <div class="breadcrumb-item"><a href="#">Forms</a></div>
@@ -26,9 +26,6 @@
 
             <div class="section-body">
                 <h2 class="section-title">Users</h2>
-
-
-
                 <div class="card">
                     <form action="{{ route('user.store') }}" method="POST">
                         @csrf
